4d4f191290212a644241cbfc54f00685e2504a21
DavouJ <djjobbi@hotmailcom>
2024-02-29 17:31:49 +0000

    connecting the search component to the api fetch logic, wrapping the searchbar component into a function as i need to add more code thus a return statement

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { AudioOutlined } from '@ant-design/icons';
 import { Input, Space } from 'antd';
-const { Search } = Input;
+/**
+ * DAVOU
+ * import_perenualFetch: Imports the js file for fetching data from the perenual API 
+ * import_{_useState,_useEffect_}: Allows us to manage state
+ */  
+import perenualFetch from '../../utils/perenualFetch'
+import { useState, useEffect } from 'react';
+
 
 const suffix = (
   <AudioOutlined
@@ -12,14 +19,19 @@ const suffix = (
   />
 );
 
-const onSearch = (value, _e, info) => console.log(info?.source, value);
 
-const SearchBar = () => (
+function SearchBar(){
+  const { Search } = Input;
+
+  const onSearch = (value, _e, info) => console.log(info?.source, value);
+
+  return(
   <Space direction="vertical">
 
     <Search placeholder="input search text" onSearch={onSearch} enterButton />
 
   </Space>
-);
+  )
+};
 
 export default SearchBar;
