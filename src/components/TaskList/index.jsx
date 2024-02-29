0ff78abe1c0fd8913d668ba3a62bc75db1c8c61a
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-02-29 10:19:21 +0000

    added functionality so that user can input new task, and on pressing save button, new task renders to list.

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Button, Input, Select, Space } from 'antd';
 import { Divider, List, Typography } from 'antd';
 
 const TaskList = () => {
 
-        const toDos = [
+    const [toDos, setToDos] = useState([
         'Re-pot pothos',
         'Clean dust off leaves of weeping fig tree',
         'Give peace lily fertilizer'
-        ];
+    ]);
+
+const [inputValue, setInputValue] = useState('');
+
+const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+}
+
+    const handleSave = () => {
+        if (inputValue.trim() !== '') {
+            setToDos([...toDos, inputValue]);
+            setInputValue('')
+        }
+    }
 
 return (
 <>
         <Divider orientation="left">Plant Tasks</Divider>
         <Space.Compact style={{ width: '100%' }}>
-            <Input placeholder="Add a task here..." />
-            <Button type="primary">Save task</Button>
+
+            <Input 
+            placeholder="Add a task here..." 
+            type="text" 
+            value={inputValue}
+            onChange={handleInputChange}/>
+
+            <Button type="primary" onClick={handleSave}>Save task</Button>
         </Space.Compact>
         <List style={{width: '100%'}}
             bordered
