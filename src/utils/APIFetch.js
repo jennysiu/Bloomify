22da28ac0c62310abfa947080a9207d606ac427c
DavouJ <djjobbi@hotmailcom>
2024-02-29 14:31:33 +0000

    fixed a small functino error

--- src/utils/APIFetch.js
+++ src/utils/APIFetch.js
@@ -2,22 +2,24 @@ const axios = require('axios');
 const fs = require("fs")
 const API_KEY = `sk-2KiZ65dfa09c6ebac4396`
 
+getPerenualResults('rose')
 
-    
-    const url = `https://perenual.com/api/species-list?key=${API_KEY}&q=rose`;
+function getPerenualResults(search) {
+    const url = `https://perenual.com/api/species-list?key=${API_KEY}&q=${search}`;
 
-axios.get(url)
-    .then((response) => {
-        
-        fs.writeFile('searchResults.JSON', JSON.stringify(response.data, null, "\t"), err => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(response.data)
-            }
+    axios.get(url)
+        .then((response) => {
+
+            fs.writeFile('searchResults.JSON', JSON.stringify(response.data, null, "\t"), err => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(response.data)
+                }
+            });
+        })
+
+        .catch((error) => {
+            //console.log(error);
         });
-    })
-
-    .catch((error) => {
-        //console.log(error);
-    });
+}
