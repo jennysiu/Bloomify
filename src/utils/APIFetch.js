609b03f8bf6b31a1e6d411afb21ad98d354c7625
DavouJ <djjobbi@hotmailcom>
2024-02-29 16:01:22 +0000

    second attempt at rebasing package

--- src/utils/APIFetch.js
+++ src/utils/APIFetch.js
@@ -2,8 +2,18 @@ const axios = require('axios');
 const fs = require("fs")
 const API_KEY = `sk-2KiZ65dfa09c6ebac4396`
 
-getPerenualResults('rose')
+getPerenualResults('bamboo')
 
+/**
+ * Retrieves results of a user search.
+ * 
+ * @async
+ * @function
+ * @param {string} search - user search
+ * @file stores search results as JSON
+ * @returns {string} path to JSON file
+ * 
+ */
 function getPerenualResults(search) {
     const url = `https://perenual.com/api/species-list?key=${API_KEY}&q=${search}`;
 
@@ -14,12 +24,17 @@ function getPerenualResults(search) {
                 if (err) {
                     console.error(err);
                 } else {
+                    
                     console.log(response.data)
+                    return path = "./houseplant-care-web-app/src/utils/searchResults.JSON"
                 }
             });
+            
         })
 
         .catch((error) => {
-            //console.log(error);
+            console.log("No searches found!");
         });
 }
+
+
