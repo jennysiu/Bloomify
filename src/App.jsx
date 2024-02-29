4c22fff7f8973f1cb381fa40f3988362717322b1
jennysiu <jenny.siu79@gmail.com>
2024-02-29 01:10:21 +0000

    footer positioning

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Dashboard, Explore, MyPlants, WateringLog, Custom404 } from './Pages'
 import './App.css'
 import Navbar from './components/Navbar'
-import SearchBar from './components/SearchBar'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -20,7 +20,10 @@ function App() {
           <Route path='/watering-log' element={< WateringLog />} />
           <Route path='/404' element={< Custom404 />} />
         </Routes>
+
+        <Footer />
       </Router>
+
     </>
   )
 }
