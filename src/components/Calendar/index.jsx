7e9b318b81df6c12117c5c16d7a194671e6f4055
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-03-01 16:49:45 +0000

    added fontawesome icons package and added water droplet to calendar entry.

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Calendar } from 'antd';
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faDroplet } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faDroplet)
+
+const droplet = icon({ prefix: 'fas', iconName: 'camera' })
 
 const getListData = ({ toDos }) => {
     return toDos.map(item => ({
@@ -19,11 +26,11 @@ const MyCalendar = ({ toDos }) => {
         });
 
         return (
-            <ul className="events">
+            <ul className="events" style={{ listStyleType: 'none' }}>
                 {tasksForDate.map((item, index) => (
                     <li key={index}>
+                        <FontAwesomeIcon icon={faDroplet} />
                         {item.content}
-                        {/* Add in waterdrop icon here? */}
                     </li>
                 ))}
             </ul>
