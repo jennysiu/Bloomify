c0dca6b7ae430081973a24c45b689cae1c44420f
DavouJ <djjobbi@hotmailcom>
2024-03-01 17:34:04 +0000

    adding filters for the serach component, including drom down menu, switches and colapsables

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,37 +1,148 @@
 import React from 'react';
-import { AudioOutlined } from '@ant-design/icons';
-import { Input, Space } from 'antd';
+import { FilterOutlined, } from '@ant-design/icons';
+import { Button, Flex, Space, Select, Input, Collapse, Checkbox } from 'antd';
 /**
  * DAVOU
  * import_perenualFetch: Imports the js file for fetching data from the perenual API 
  * import_{_useState,_useEffect_}: Allows us to manage state
- */  
+ */
 import perenualFetch from '../../utils/perenualFetch'
 import { useState, useEffect } from 'react';
 
 
-const suffix = (
-  <AudioOutlined
-    style={{
-      fontSize: 16,
-      color: '#1677ff',
-    }}
-  />
-);
 
 
-function SearchBar(){
+function SearchBar() {
+  /**
+ * setName hooks for storing dynamically changing user input
+ * {Search} - ant design?
+ * setCollapsed - ant design hook for  collapsible filter
+ */
+  const [name, setName] = useState('');
+  const [watering, setWatering] = useState('');
+  const [sunlight, setSunlight] = useState('');
+  const [isIndoors, setIsindoors] = useState('');
+
   const { Search } = Input;
+  const [collapsed, setCollapsed] = useState(false);
 
-  const onSearch = (value, _e, info) => console.log(info?.source, value);
+  const wateringOption = ['frequent', 'average', 'minimum', 'none']
+  const sunlightOption = ['full_shade', 'part_shade', 'sun-part_shade', 'full_sun']
 
-  return(
-  <Space direction="vertical">
+  // Function that prevent effects when user hasnt searched
+  useEffect(() => {
+    if (!name) {
+      return;
+    }
+  });
 
-    <Search placeholder="input search text" onSearch={onSearch} enterButton />
+  const onChange = (value) => {
+    //console.log(value)
+    wateringOption.find((element) => element === value) ? setWatering(value): setWatering('');
+    sunlightOption.find((element) => element === value) ? setSunlight(value): setSunlight('');
+    
+  }
 
-  </Space>
-  )
-};
 
-export default SearchBar;
+    const onSearch = (value, _e, info) => {
+
+      perenualFetch.getPerenualResults(value)
+        .then((res) => {
+          console.log(res.data)
+            .catch(err => {
+              console.log(err);
+            });
+        });
+    };
+
+    const filterOption = (input, option) =>
+      (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+
+    const selectSunlight =
+      <Select
+        showSearch
+        placeholder="Sunlight"
+        optionFilterProp="children"
+        onChange={onChange}
+        filterOption={filterOption}
+        options={[
+          {
+            value: sunlightOption[0],
+            label: 'Full Sun',
+          },
+          {
+            value: sunlightOption[1],
+            label: 'Sun/Part Shade',
+          },
+          {
+            value: sunlightOption[2],
+            label: 'Part Shade',
+          },
+          {
+            value: sunlightOption[3],
+            label: 'Full Shade',
+          },
+        ]}
+      />
+    const selectWatering =
+      <Select
+        showSearch
+        placeholder="Watering"
+        optionFilterProp="children"
+        onChange={onChange}
+        filterOption={filterOption}
+        options={[
+          {
+            value: wateringOption[0],
+            label: 'Often',
+          },
+          {
+            value: wateringOption[1],
+            label: 'Average',
+          },
+          {
+            value: wateringOption[2],
+            label: 'Barely',
+          },
+          {
+            value: wateringOption[3],
+            label: 'None',
+          },
+        ]}
+      />
+
+    const filterItems = [
+      {
+        key: '1',
+        label: 'More Filters',
+        children:
+          <Space direction="vertical">
+            <Checkbox name='indoorChk' onChange={onChange}>Indoor</Checkbox>
+            <Checkbox name='outdoorChk' onChange={onChange}>Outdoor</Checkbox>
+            <Space direction="horizontal">
+              <p>Watering</p>
+              {selectWatering}
+            </Space>
+            <Space direction="horizontal">
+              <p>Sunlight</p>
+              {selectSunlight}
+            </Space>
+            <Button type="primary" onClick={onSearch} >Advanced Search</Button>
+          </Space>,
+      }
+    ];
+
+
+    return (
+      <Space direction="vertical">
+
+        <Search placeholder="Search for Plant" onSearch={onSearch} />
+
+        <Collapse items={filterItems} onChange={onChange} />
+
+
+      </Space>
+    )
+  };
+
+  export default SearchBar;
