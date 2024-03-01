b3920ea3c4f7b90f64c0d938ecdccfd8de709484
jennysiu <jenny.siu79@gmail.com>
2024-03-01 23:55:15 +0000

    a few component tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -2,7 +2,21 @@ import { render, screen } from '@testing-library/react';
 import * as userEvent from '@testing-library/user-event';
 import App from '../App';
 
-// Test 1: Write a test that checks to see if our `App` component renders without throwing an error.
+import { MemoryRouter } from 'react-router-dom';
+
+
+// test that checks to see if our `App` component renders without throwing an error.
 it('App Component Renders Without Error', () => {
   render(<App />);
 });
+
+// routing testing
+it("renders the dashboard component for the root path", () => {
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText("Dashboard Content")).toBeInTheDocument();
+})
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// internal imports
+import '../Header/style.css';
+
 
 const Header = () => {
   return (
