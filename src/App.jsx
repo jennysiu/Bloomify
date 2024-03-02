68c0ef8c707ce312189eed28910f798ecf12ee7e
jennysiu <jenny.siu79@gmail.com>
2024-03-02 13:54:08 +0000

    header now renders

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { Layout } from 'antd';
 import { Dashboard, Explore, MyPlants, WateringLog, Custom404 } from './pages/index.jsx'
 import SearchResults from './pages/SearchResults.jsx'
 import './App.css'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
+import Navbar from './components/CustomNavbar/index.jsx'
+import CustomHeader from './components/CustomHeader'
 import Footer from './components/Footer'
 import { ToDoProvider } from './contexts/ContextsToDos';
 import { LocationProvider } from './contexts/ContextLocation'
@@ -18,7 +18,7 @@ function App() {
   const [collapsed, setCollapsed] = useState(false); 
 
   // Import Layout components here
-const { Header, Content, Footer, Sider } = Layout;
+  const { Content, Footer, Sider } = Layout;
 
   return (
     <>
@@ -26,33 +26,33 @@ const { Header, Content, Footer, Sider } = Layout;
         {/* The whole app must be wrapped in the ToDoProvider so that the Watering Log page can access it */}
         <ToDoProvider>
             <MyPlantsProvider>
-            <LocationProvider>
-      <Layout style={{ minHeight: '100vh' }}>
-            <Layout style={{ minHeight: '100vh' }}>
-              <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
-                <Navbar collapsed={collapsed} setCollapsed={setCollapsed}/>
-              </Sider>
+              <LocationProvider>
+                <Layout style={{ minHeight: '100vh' }}>
+                      <Layout style={{ minHeight: '100vh' }}>
+                          <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+                            <Navbar collapsed={collapsed} setCollapsed={setCollapsed}/>
+                          </Sider>
 
-              <Layout>
-                <Header />
+                          <Layout>
+                            <CustomHeader />
 
-                <Content style={{ margin: '16px', padding: 24, background: '#fff', minHeight: 360 }}>
-                  <Routes>
-                    <Route path='' element={< Dashboard />} />
-                    <Route path='/dashboard' element={< Dashboard />} />
-                    <Route path='/explore' element={< Explore />} />
-                    <Route path='/my-plants' element={< MyPlants />} />
-                    <Route path='/watering-log' element={< WateringLog />} />
-                    <Route path='/search-results' element={< SearchResults/>} />
-                    <Route path='/404' element={< Custom404 />} />
-                  </Routes>
-                </Content>
+                            <Content style={{ margin: '16px', padding: 24, background: '#fff', minHeight: 360 }}>
+                              <Routes>
+                                <Route path='' element={< Dashboard />} />
+                                <Route path='/dashboard' element={< Dashboard />} />
+                                <Route path='/explore' element={< Explore />} />
+                                <Route path='/my-plants' element={< MyPlants />} />
+                                <Route path='/watering-log' element={< WateringLog />} />
+                                <Route path='/search-results' element={< SearchResults/>} />
+                                <Route path='/404' element={< Custom404 />} />
+                              </Routes>
+                            </Content>
 
-          <Footer />
-        </Layout>
-      </Layout>
-      </Layout>
-          </LocationProvider>
+                            <Footer />
+                          </Layout>
+                  </Layout>
+                  </Layout>
+              </LocationProvider>
             </MyPlantsProvider>
         </ ToDoProvider>
     </Router>
