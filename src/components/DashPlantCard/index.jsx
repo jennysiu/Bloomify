03166d5587ec41af8885531875d392cd4789ff95
jennysiu <jenny.siu79@gmail.com>
2024-03-02 15:18:32 +0000

    added dummy data for dashplant card

--- src/components/DashPlantCard/index.jsx
+++ src/components/DashPlantCard/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'antd';
 
-const DashPlantCard = ({ plant }) => {
+const DashPlantCard = ({ plant , index}) => {
     // Display logic using plant data
-    return <Card title={plant.common_name}> {/* Display plant details here */} </Card>;
+    return (
+    <>
+        <Card title={plant.common_name}> {/* Display plant details here */} 
+            <img src={plant.default_image.regular_url} alt={plant.common_name} style={{ width: 200, height: 200 }} />
+            <div key={index} style={{ marginTop: '20px', fontWeight: 'bold' }}>{plant.common_name}</div>
+        </Card>
+    </>
+    )
 };
 
-DashPlantCard.propTypes = {
-    plant: PropTypes.object.isRequired,
-};  
+// DashPlantCard.propTypes = {
+//     plant: PropTypes.object.isRequired,
+// };  
 
 export default DashPlantCard;
