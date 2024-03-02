04d22180383ba0e38360f0c2981deb21f7564612
Laura Kane <148717413+kauralane@users.noreply.github.com>
2024-03-02 17:40:42 +0000

    added search function to weather widget so user can search for a location as well as option to use geolocation. Work in progress --> currently the user has to refresh the page for the correct location's weather to render.

--- src/components/WeatherWidget/index.jsx
+++ src/components/WeatherWidget/index.jsx
@@ -2,8 +2,27 @@ import React, { useState, } from 'react';
 import { Button, Modal } from 'antd';
 import { LocationProvider, LocationContext } from '../../contexts/ContextLocation'
 import weatherAPIfetch from '../../utils/weatherAPIfetch';
+import { Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+import searchWeatherAPIfetch from '../../utils/searchWeatherAPIfetch';
+const { Search } = Input;
+
+const suffix = (
+    <AudioOutlined
+        style={{
+            fontSize: 16,
+            color: '#1677ff',
+        }}
+    />
+);
+
+// const onSearch = (value, _e, info) => console.log(info?.source, value);
 
 const WeatherWidget = ({ location, setLocation }) => {
+
+    const onSearch = (value) => {searchWeatherAPIfetch({ location: { location }, setLocation: {       setLocation }, value })
+
+}
 
     const [isWeatherFetched, setIsWeatherFetched] = useState(false)
 
@@ -23,15 +42,16 @@ const WeatherWidget = ({ location, setLocation }) => {
         const storedLocation = [{ latitude: latitude, longitude: longitude }]
         setLocation({ storedLocation });
 
-        console.log(`Latitude: ${latitude} Longitude: ${longitude}`);
+        // save to local storage
         localStorage.setItem('location', JSON.stringify(storedLocation));
 
         weatherAPIfetch({ location: storedLocation, setLocation })
-            .then(setIsWeatherFetched(true));
-
+        .then(setIsWeatherFetched(true))
     }
 
-    // Stores weather data to local storage
+
+
+    // Gets weather data from local storage
     const storedWeather = localStorage.getItem('weather')
     const weather = JSON.parse(storedWeather)
 
@@ -43,20 +63,23 @@ const WeatherWidget = ({ location, setLocation }) => {
     return (
         <>
             <LocationProvider>
-                <Button onClick={() => setIsWeatherFetched(true)}>Show Latest Weather</Button>
-                <span>
-                    {weather ? (
-                        <>
-                            <h1>Showing weather in {weather.name}</h1>
-                            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
-                            <p>Description: {weather.weather[0].description}</p>
-                            <p>Temperature: {weather.main.temp}</p>
-                            <p>Humidity: {weather.main.humidity} %</p>
-                            <p>Rain: {weather.rain}</p>
-                        </>
-                    ) : null}
-                </span>
-                <Button onClick={getUserLocation}>Change/set current location</Button>
+                <Search placeholder="Search for a location" onSearch={onSearch} style={{ width: 200 }} />
+                <br></br>
+                <Button onClick={getUserLocation}>Or use your geolocation</Button>
+                <br></br>
+                    {/* <Button onClick={() => setIsWeatherFetched(true)}>Show Latest Weather</Button> */}
+                    <span>
+                        {weather ? (
+                            <>
+                                <h1>Showing weather in {weather.name}</h1>
+                                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+                                <p>Description: {weather.weather[0].description}</p>
+                                <p>Temperature: {weather.main.temp}</p>
+                                <p>Humidity: {weather.main.humidity} %</p>
+                                <p>Rain: {weather.rain}</p>
+                            </>
+                        ) : null}
+                    </span>
             </LocationProvider>
         </>
     )
