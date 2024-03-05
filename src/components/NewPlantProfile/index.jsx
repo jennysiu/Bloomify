e6b4601de98c62746f62c6618acc585b38f99f14
jennysiu <jenny.siu79@gmail.com>
2024-03-05 19:30:54 +0000

    made width smaller so there's less whitespace

--- src/components/NewPlantProfile/index.jsx
+++ src/components/NewPlantProfile/index.jsx
@@ -49,7 +49,7 @@ function NewPlantProfile({ selectedPlantModalVisible, toggleNewPlantProfVisi, se
     centered
     open={selectedPlantModalVisible}
     onCancel={handleClose}
-    width={1000}
+    width={600}
     footer={[
       // <Popconfirm
       // key="add-plant"
