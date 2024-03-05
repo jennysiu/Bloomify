e466488d9c54ce5211e9936384d19af551e06f0d
DavouJ <djjobbi@hotmailcom>
2024-03-05 08:39:01 +0000

    added fetch call to plant of the day component

--- src/components/PlantOfTheDay/index.jsx
+++ src/components/PlantOfTheDay/index.jsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card } from 'antd';
 import placeholderPlantImage from '../../assets/images/placeholder-img.png';
 import './style.css'
-import perenualFetch from '../../utils/perenualFetch'
+import plantDetailsFetch from '../../utils/plantDetailsFetch'
+
 
 
 
 const PlantOfTheDay = () => {
-
-    //const
-
-    // Static placeholder data for now
-    const plant = {
-        name: "Placeholder Plant",
-        imageUrl: placeholderPlantImage
-    };
-
-    function hasOneDayPassed(){
-    var date = new Date().toLocaleDateString();
     
-    if (localStorage.yourapp_date == date)
-        return false;
+   runOncePerDay  
 
-    localStorage.yourapp_date = date;
-    return true;
+    useEffect(() => {
+       
+    });
+
+
+    function hasOneDayPassed() {
+        var date = new Date().toLocaleDateString();
+
+        if (localStorage.yourapp_date == date)
+            return false;
+
+        localStorage.yourapp_date = date;
+        return true;
     }
 
 
 
-function runOncePerDay() {
-    if (!hasOneDayPassed()) return false;
+    function runOncePerDay() {
+        if (!hasOneDayPassed()) return false;
 
-    
-    alert('Good morning!');
-}
+        plantDetailsFetch.getPlantDetails(Math.floor(Math.random() * 3000))
+            .then((data) => {
+                console.log(data)
+                localStorage.setItem("nameKey", data.data.common_name)
+                localStorage.setItem("descKey", data.data.type)
+                localStorage.setItem("imgKey", data.data.default_image.original_url)
+                this.forceUpdate();
+            })
+            .catch(() => {
+                console.error();
+            });
+    }
+
 
 
     return (
         <Card
             title="Plant of the Day"
-            cover={<img alt={plant.name} src={plant.imageUrl} className="plant-image-cover" />}
-            
+            cover={<img alt={localStorage.getItem("nameKey")} src={localStorage.getItem("imgKey")} className="plant-image-cover" />}
+
         >
-            <Card.Meta title={plant.name} description={plant.description} />
+            <Card.Meta title={localStorage.getItem("nameKey")} description={localStorage.getItem("descKey")} />
         </Card>
     );
 };
