3c2a692c80e793bd5ba3a82970ef3b778f603136
jennysiu <jenny.siu79@gmail.com>
2024-03-06 23:49:16 +0000

    fields are capitalised and with commas

--- src/components/PlantProfile/index.jsx
+++ src/components/PlantProfile/index.jsx
@@ -6,6 +6,8 @@ import "./style.css"
 
 function PlantProfile({ selectedPlantModalVisible, togglePlantProfileVisibility, selectedPlantModalPlant, myPlants, setMyPlants }) {
   let plantData = selectedPlantModalPlant;
+
+  console.log(plantData.type);
   
   const handleClose = () => {
     togglePlantProfileVisibility(false);
@@ -47,112 +49,139 @@ function PlantProfile({ selectedPlantModalVisible, togglePlantProfileVisibility,
       <Button key="submit" type="primary" onClick={handleClose}>
       Close
       </Button>
-
     ]}
     >
 
     <div className='plant-profile-modal-content'>
-    <img src={plantData.default_image.small_url} alt={`Picture of ${plantData.common_name} plant`} style={{ maxWidth: '100%', borderRadius: '2%' }} />
-    <Tabs
-        defaultActiveKey="1"
-        items={[
-          {
-            label: 'Basic Overview',
-            key: '1',
-            children: 
-            (<div>
-              {/* indoor tag */}
-              {plantData.indoor === true ? (
-              <Tag bordered={false} color="green">
-                Indoor
-              </Tag>) 
-              : plantData.indoor === false ? (
-              <Tag bordered={false} color="magenta">
-                Outdoor
-              </Tag>)
-              : null}
-              {/* maintenance tags */}
-              {plantData.maintenance === "Low" ? (
+      <h4>({plantData.scientific_name})</h4>
+      <img src={plantData.default_image.small_url} alt={`Picture of ${plantData.common_name} plant`} style={{ maxWidth: '100%', borderRadius: '2%' }} />
+      <Tabs
+          defaultActiveKey="1"
+          items={[
+            {
+              label: 'Basic Overview',
+              key: '1',
+              children: 
+              (<div>
+                {/* indoor tag */}
+                {plantData.indoor === true ? (
                 <Tag bordered={false} color="green">
-                  Low maintenance
-                </Tag>
-              ) : plantData.maintenance === "Moderate" ? (
-                <Tag bordered={false} color="orange"> 
-                  Moderate maintenance 
-                </Tag> 
-              ) : plantData.maintenance === "High" ? (
-                <Tag bordered={false} color="red"> 
-                  High maintenance
-                </Tag>
-              ) : null}   
-              {/* watering tag */}
-              {plantData.watering === "None" ? (
-                <Tag bordered={false} color="green">
-                  No watering
-                </Tag>
-              ) : plantData.watering === "Minimum" ? (
-                <Tag bordered={false} color="green"> 
-                  Minimum watering 
-                </Tag> 
-              ) : 
-              plantData.watering === "Average" ? (
-                <Tag bordered={false} color="blue"> 
-                  Average watering 
-                </Tag> 
-              ) : plantData.watering === "Frequent" ? (
-                <Tag bordered={false} color="red"> 
-                  Frequent watering
-                </Tag>
-              ) : null}   
-
-              <p>Watering frequency: {plantData.watering_general_benchmark.value} {plantData.watering_general_benchmark.unit}</p>
-              <p>Sunlight: {plantData.sunlight}</p> 
-              <p>Cycle: {plantData.cycle}</p> 
-              <p>Origin: {plantData.origin}</p>
-            </div>),
-          },
-          {
-            label: 'Care & growth',
-            key: '2',
-            children: (
-              <div>
-                <h4>Care</h4>
-                <p>Hardiness: {plantData.hardiness.min} - {plantData.hardiness.max}</p>
-                <p>Best watering period: {plantData.watering_period ? plantData.watering_period : "n/a"}</p>
-                <p>Tropical: {plantData.tropical === "true" ? (
-                  <Tag>
-                    Yes
+                  Indoor
+                </Tag>) 
+                : plantData.indoor === false ? (
+                <Tag bordered={false} color="magenta">
+                  Outdoor
+                </Tag>)
+                : null}
+                {/* maintenance tags */}
+                {plantData.maintenance === "Low" ? (
+                  <Tag bordered={false} color="green">
+                    Low maintenance
                   </Tag>
-                  ) : 
-                  (<Tag>
-                    No
+                ) : plantData.maintenance === "Moderate" ? (
+                  <Tag bordered={false} color="orange"> 
+                    Moderate maintenance 
+                  </Tag> 
+                ) : plantData.maintenance === "High" ? (
+                  <Tag bordered={false} color="red"> 
+                    High maintenance
                   </Tag>
-                )}</p>
+                ) : null}   
+                {/* watering tag */}
+                {plantData.watering === "None" ? (
+                  <Tag bordered={false} color="green">
+                    No watering
+                  </Tag>
+                ) : plantData.watering === "Minimum" ? (
+                  <Tag bordered={false} color="green"> 
+                    Minimum watering 
+                  </Tag> 
+                ) : 
+                plantData.watering === "Average" ? (
+                  <Tag bordered={false} color="blue"> 
+                    Average watering 
+                  </Tag> 
+                ) : plantData.watering === "Frequent" ? (
+                  <Tag bordered={false} color="red"> 
+                    Frequent watering
+                  </Tag>
+                ) : null}   
 
-                <h4>Growth</h4>
+                <p>Watering frequency: {plantData.watering_general_benchmark.value} {plantData.watering_general_benchmark.unit}</p>
+                <p>Sunlight: {plantData.sunlight && plantData.sunlight.map((s, index) => (
+                <span key={index}>{s.charAt(0).toUpperCase() + s.slice(1)}{index < plantData.sunlight.length - 1 ? ', ' : ''}</span>
+                ))}
+                </p>
                 <p>Cycle: {plantData.cycle}</p> 
-                <p>Flowering season: {plantData.flowering_season}</p>
-                <p>Growth rate: {plantData.growth_rate}</p>
+                <p>Sunlight: {plantData.origin && plantData.origin.map((s, index) => (
+                <span key={index}>{s.charAt(0).toUpperCase() + s.slice(1)}{index < plantData.origin.length - 1 ? ', ' : ''}</span>
+                ))}
+                </p>
+              </div>),
+            },
+            {
+              label: 'Care & growth',
+              key: '2',
+              children: (
+                <div>
+                  <h4>Care</h4>
+                  <p>Hardiness: {plantData.hardiness.min} - {plantData.hardiness.max}</p>
+                  <p>Best watering period: {plantData.watering_period ? plantData.watering_period : "n/a"}</p>
+                  <p>Tropical: {plantData.tropical === "true" ? (
+                    <Tag>
+                      Yes
+                    </Tag>
+                    ) : 
+                    (<Tag>
+                      No
+                    </Tag>
+                  )}</p>
 
-                <p>Recommended pruning month: {plantData.pruning_month}</p> 
-                <p>Proppogationg: {plantData.propagation} </p> 
-              </div>
-            )
-          },
-          {
-            label: 'Additional info',
-            key: '3',
-            children: (
-              <div>
-                <p>Scientific name: {plantData.scientific_name}</p> 
-                <p>Family: {plantData.family}</p>
-                <p>Type: {plantData.type}</p> 
-                <p>Description: {plantData.description}</p>
-              </div>
-            )
-          }
-        ]}
-    />
+                  <h4>Growth</h4>
+                  <p>Cycle: {plantData.cycle}</p> 
+                  <p>Flowering season: {plantData.flowering_season ? plantData.flowering_season: "n/a"}</p>
+                  <p>Growth rate: {
+                    plantData.growth_rate === "Low" ? (
+                    <Tag bordered={false} color="green"> 
+                      Low 
+                    </Tag> 
+                    ) : 
+                    plantData.growth_rate === "Average" ? (
+                      <Tag bordered={false} color="blue"> 
+                        Average
+                      </Tag> 
+                    ) : plantData.growth_rate === "High" ? (
+                      <Tag bordered={false} color="red"> 
+                        High
+                      </Tag>
+                    ) : null}  
+                  </p>    
+
+                  <p>Recommended pruning month: {plantData.pruning_month && plantData.pruning_month.map((s, index) => (
+                  <span key={index}>{s}{index < plantData.pruning_month.length - 1 ? ', ' : ''}</span>
+                  ))}
+                  </p>
+                  <p>Propogationg methods: {plantData.propagation && plantData.propagation.map((s, index) => (
+                  <span key={index}>{s}{index < plantData.propagation.length - 1 ? ', ' : ''}</span>
+                  ))}
+                  </p>
+                </div>
+              )
+            },
+            {
+              label: 'Additional info',
+              key: '3',
+              children: (
+                <div>
+                  <p>Scientific name: {plantData.scientific_name}</p> 
+                  <p>Family: {plantData.family ? plantData.family  : "N/A"}</p>
+                  <p>Type: {plantData.type.charAt(0).toUpperCase() + plantData.type.slice(1)}</p>
+                  <p>Description: {plantData.description}</p>
+                </div>
+              )
+            }
+          ]}
+      />
     </div>
     </Modal>
  
