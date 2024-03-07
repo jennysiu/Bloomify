ecaa6556279c7fe8d8ee08bbb15f40bb2573a179
jennysiu <jenny.siu79@gmail.com>
2024-03-07 17:54:43 +0000

    image border radius and margins

--- src/components/PlantCard/index.jsx
+++ src/components/PlantCard/index.jsx
@@ -1,5 +1,8 @@
 import { Card } from 'antd';
 
+// internal imports
+import "./style.css";
+
 
 function PlantCard( {plant, index, style} ) {
     // console.log(plant)
@@ -10,10 +13,14 @@ function PlantCard( {plant, index, style} ) {
 
     return (
         <>
+        <div className='plant-car-container'>
             <Card className="plant-card" style={style}>
-                <img src={plant.default_image.regular_url} alt={plant.common_name} style={{ width: '80%', height: 'auto', maxHeight: '200px', objectFit: 'cover'}} />
+                <img 
+                className='plant-card-img'
+                src={plant.default_image.regular_url} alt={plant.common_name} style={{ width: '80%', height: 'auto', maxHeight: '200px', objectFit: 'cover'}} />
                 <h4 key={index} style={{ marginTop: '20px', fontWeight: 'bold' }}>{plant.common_name.charAt(0).toUpperCase() + plant.common_name.slice(1)}</h4>
             </Card>
+        </div>
         </>
     )
 }
