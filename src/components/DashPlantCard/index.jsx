97c637bb32aa4779d2abb05d7a482ff34ed857d5
jennysiu <jenny.siu79@gmail.com>
2024-03-15 18:43:01 +0000

    re-arranged dashboard

--- src/components/DashPlantCard/index.jsx
+++ src/components/DashPlantCard/index.jsx
@@ -11,7 +11,10 @@ const DashPlantCard = ({ myPlants, setMyPlants }) => {
     const map = () => {
         for (const index in myPlants) {
             console.log(myPlants.index)
-            plants.push(<img key={index} src={myPlants[index].default_image.original_url} alt={myPlants[index].common_name} />)
+            plants.push(<img key={index} 
+                src={myPlants[index].default_image.original_url} 
+                alt={myPlants[index].common_name} 
+                />)
         }
     }
 
@@ -19,12 +22,15 @@ const DashPlantCard = ({ myPlants, setMyPlants }) => {
 
     return (
         <div className="dashPlantCardContainer">
-                <h2>My Plants</h2>
+            <h2>My Plants</h2>
+            <Card>
                 <div className="dashPlantCardContent" >
                 {plants} 
                 </div>
+            </Card>
         </div>
     );
 };
+
 
 export default DashPlantCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
   return (
     <>
       <SearchBar />
-      <Divider />
+      <Divider className='divider'/>
       {/* PLACEHOLDER _ SMALL LEAF IMAGE */}
 
       <div id="dashboard-container"
@@ -47,20 +47,18 @@ const Dashboard = () => {
             <Col sm={23} md={14} 
             style={{ marginLeft: 8, marginRight: 8 }}>
               <Row gutter={16} style={{ marginBottom: 16 }}>
-          <h1 id='dash-header' >Welcome back, {userName || 'Guest'}!</h1>
+                <h1 id='dash-title' >Welcome back, {userName || 'Guest'} 😊</h1>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} 
+                style={{ marginBottom: 16 }}>
+                  <PlantOfTheDay />
+                </Col>
                 <Col xs={24} sm={24} md={24} lg={24} xl={24} style={{ marginBottom: 16 }}>
                   {/* Assuming DashPlantCard is to be rendered for each plant. Adjust if it's just one card. */}
                   <DashPlantCard myPlants={myPlants} />
                 </Col>
               </Row>
               <Row gutter={16}>
-                <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ marginBottom: 16 }}>
-                  <WeatherWidget location={location} setLocation={setLocation} />
-                </Col>
 
-                <Col xs={24} sm={24} md={12} lg={12} xl={12} style={{ marginBottom: 16 }}>
-                  <PlantOfTheDay />
-                </Col>
 
                   
               </Row>
@@ -73,6 +71,9 @@ const Dashboard = () => {
             <Col sm={24} md={8} 
             style={{ marginLeft: 8, marginRight: 8 }}>
               <Row gutter={16} style={{ marginBottom: 16 }}>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} style={{ marginBottom: 16 }}>
+                  <WeatherWidget location={location} setLocation={setLocation} />
+                </Col>
               
                 <Col xs={24} sm={24} md={24} lg={24} xl={24} style={{ marginBottom: 16 }}>
                   <ToDoProvider>
