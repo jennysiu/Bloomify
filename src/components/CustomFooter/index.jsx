915115ed738b143474a6ed66d1f0ca2970da0af1
jennysiu <jenny.siu79@gmail.com>
2024-03-20 19:54:09 +0000

    updated colour and sizing

--- src/components/CustomFooter/index.jsx
+++ src/components/CustomFooter/index.jsx
@@ -1,4 +1,4 @@
-import { Flex, Row } from 'antd';
+import { Flex, Row, Col } from 'antd';
 import './style.css'
 
 function Footer () {
@@ -6,17 +6,21 @@ function Footer () {
         <>
             <div className='footer'>
                 <Flex align="center" justify="center">
-                    <Row>
-                        <p className="text">2024 © All Rights Reserved.</p>
-                    </Row>
+                    {/* <Row> */}
+                    {/* </Row> */}
                         </Flex>
                         <Flex align="center" justify="center">
                     <Row>
+                        <Col>
                         <a href="https://github.com/adriannaderkacz" className="link">Adrianna Derkacz</a>
                         <a href="https://github.com/ahmed-ibrahim20560" className="link">Ahmed Ibrahim</a>
                         <a href="https://github.com/DavouJ" className="link">Davou Jobi</a>
                         <a href="https://github.com/kauralane" className="link">Laura Kane</a>
                         <a href="https://github.com/jennysiu" className="link">Jenny Siu</a>
+                        </Col>
+                        <Col>
+                        <p className="copy-right-text">2024 © All Rights Reserved.</p>
+                        </Col>
                     </Row>
                 </Flex >
             </div>
