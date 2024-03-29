a2b45688f990cbfc53c60a5b37aff9cd5c16acd6
jennysiu <jenny.siu79@gmail.com>
2024-03-29 00:50:38 +0000

    footer styling theme updated

--- src/components/CustomFooter/index.jsx
+++ src/components/CustomFooter/index.jsx
@@ -1,4 +1,7 @@
 import { Flex, Row, Col } from 'antd';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
 import './style.css'
 
 function Footer () {
@@ -6,15 +9,25 @@ function Footer () {
         <>
             <div className='footer'>
                     <Row className='footer-row'> 
-                        <Col className='copyright'>
-                        <p className="copy-right-text">2024 © All Rights Reserved.</p>
+                        <Col className='social-links'>
+                            <a href="https://github.com/jennysiu">
+                            <i className="fa-brands fa-github"></i>
+                            </a>
+                            <a href="https://www.linkedin.com/in/jenny-siu-534576156/">
+                            <i className="fa-brands fa-linkedin"></i>
+                            </a>
+                            <a href="mailto:jenny.siu79@gmail.com">
+                            <i className="fa-solid fa-envelope"></i>
+                            </a>
                         </Col>
-                        <Col>
-                        <a href="https://github.com/jennysiu" className="link">Jenny Siu</a>
-                        <a href="https://github.com/adriannaderkacz" className="link">Adrianna Derkacz</a>
-                        <a href="https://github.com/ahmed-ibrahim20560" className="link">Ahmed Ibrahim</a>
-                        <a href="https://github.com/DavouJ" className="link">Davou Jobi</a>
-                        <a href="https://github.com/kauralane" className="link">Laura Kane</a>
+
+                        <Col className='team-names'>
+                        <a href="https://github.com/jennysiu" className="team-link">Jenny Siu</a>
+                        <a href="https://github.com/adriannaderkacz" className="team-link">Adrianna Derkacz</a>
+                        <a href="https://github.com/ahmed-ibrahim20560" className="team-link">Ahmed Ibrahim</a>
+                        <a href="https://github.com/DavouJ" className="team-link">Davou Jobi</a>
+                        <a href="https://github.com/kauralane" className="team-link">Laura Kane</a>
+                        <p className="copy-right-text">2024 © All Rights Reserved.</p>
                         </Col>
                     </Row>
             </div>
